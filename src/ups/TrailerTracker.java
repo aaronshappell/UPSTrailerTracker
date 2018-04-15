360928cecdd5b79361445037718b19ca08afa0cc
aaronshappell <crumster88@gmail.com>
2018-04-15 13:57:05 -0700

    Read excel file test

--- src/ups/TrailerTracker.java
+++ src/ups/TrailerTracker.java
@@ -1,7 +1,20 @@
 package ups;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.File;
+import java.io.IOException;
+
 public class TrailerTracker {
-    public static void main(String[] args){
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws IOException, InvalidFormatException {
+        Workbook workbook = WorkbookFactory.create(new File("./data/test.xlsx"));
+        System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets.");
+        Sheet sheet = workbook.getSheetAt(0);
+        for(Row row : sheet){
+            for(Cell cell : row){
+                System.out.println(cell.getStringCellValue());
+            }
+        }
     }
 }
