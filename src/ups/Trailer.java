fcfbe1c5633d63455aae5f43cc587ebb749726b6
Ryan Shappell <hoovcat99@gmail.com>
2018-04-14 17:21:19 -0700

    Add base project

---
+++ src/ups/Trailer.java
@@ -0,0 +1,29 @@
+package ups;
+
+public class Trailer {
+    private String id;
+    private int origin;
+
+    //Volume Mix
+    private int volume;
+    private int smalls;
+    private int bags;
+    private int handles;
+
+    //Plan
+    private double planHours;
+    private double planStart;
+    private double planFinish;
+
+    private int door;
+    private double trailerPercentage;
+    private double start;
+    private double finish;
+    private double clearCall;
+    private boolean loadQuality;
+
+    //Progress in minutes
+    private double min30;
+    private double min60;
+    private double min90;
+}
