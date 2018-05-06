7a977a9b6b07d4c77bc36ebb321b7259f0556056
SergeyAK47 <38388649+SergeyAK47@users.noreply.github.com>
2018-05-05 19:23:44 -0700

    Getters and Setters

--- src/main/java/com/ups/UPSTrailerTracker/Trailer.java
+++ src/main/java/com/ups/UPSTrailerTracker/Trailer.java
@@ -71,5 +71,149 @@ public class Trailer {
         //Column 5 : Number of Handles : Double
         //Column 6 : Planned Hours : Double
     }
+
+    public int getIdentificationNumber() {
+        return identificationNumber;
+    }
+
+    public int getOriginNumber() {
+        return originNumber;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public int getSmalls() {
+        return smalls;
+    }
+
+    public int getBags() {
+        return bags;
+    }
+
+    public int getHandles() {
+        return handles;
+    }
+
+    public double getPlanHours() {
+        return planHours;
+    }
+
+    public double getPlanStart() {
+        return planStart;
+    }
+
+    public double getPlanFinish() {
+        return planFinish;
+    }
+
+    public int getBayDoorNumber() {
+        return bayDoorNumber;
+    }
+
+    public double getCurrentPercentage() {
+        return currentPercentage;
+    }
+
+    public double getActualStart() {
+        return actualStart;
+    }
+
+    public boolean isWarningReady() {
+        return warningReady;
+    }
+
+    public double getActualFinish() {
+        return actualFinish;
+    }
+
+    public double getMinute30() {
+        return minute30;
+    }
+
+    public double getMinute60() {
+        return minute60;
+    }
+
+    public double getMinute90() {
+        return minute90;
+    }
+
+    public double getTimeCalledClear() {
+        return timeCalledClear;
+    }
+
+    public void setIdentificationNumber(int identificationNumber) {
+        this.identificationNumber = identificationNumber;
+    }
+
+    public void setOriginNumber(int originNumber) {
+        this.originNumber = originNumber;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setSmalls(int smalls) {
+        this.smalls = smalls;
+    }
+
+    public void setBags(int bags) {
+        this.bags = bags;
+    }
+
+    public void setHandles(int handles) {
+        this.handles = handles;
+    }
+
+    public void setPlanHours(double planHours) {
+        this.planHours = planHours;
+    }
+
+    public void setPlanStart(double planStart) {
+        this.planStart = planStart;
+    }
+
+    public void setPlanFinish(double planFinish) {
+        this.planFinish = planFinish;
+    }
+
+    public void setBayDoorNumber(int bayDoorNumber) {
+        this.bayDoorNumber = bayDoorNumber;
+    }
+
+    public void setCurrentPercentage(double currentPercentage) {
+        this.currentPercentage = currentPercentage;
+    }
+
+    public void setActualStart(double actualStart) {
+        this.actualStart = actualStart;
+    }
+
+    public void setWarningReady(boolean warningReady) {
+        this.warningReady = warningReady;
+    }
+
+    public void setActualFinish(double actualFinish) {
+        this.actualFinish = actualFinish;
+    }
+
+    public void setMinute30(double minute30) {
+        this.minute30 = minute30;
+    }
+
+    public void setMinute60(double minute60) {
+        this.minute60 = minute60;
+    }
+
+    public void setMinute90(double minute90) {
+        this.minute90 = minute90;
+    }
+
+    public void setTimeCalledClear(double timeCalledClear) {
+        this.timeCalledClear = timeCalledClear;
+    }
 }
 
