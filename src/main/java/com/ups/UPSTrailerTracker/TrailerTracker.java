1dd46505c195b8503d5a1351108ce10773f4f6f5
SergeyAK47 <38388649+SergeyAK47@users.noreply.github.com>
2018-05-05 17:54:15 -0700

    Traversed File Succesfully

--- src/main/java/com/ups/UPSTrailerTracker/TrailerTracker.java
+++ src/main/java/com/ups/UPSTrailerTracker/TrailerTracker.java
@@ -16,32 +16,98 @@ public class TrailerTracker {
     }
 
     public void loadExcelFile(String excelFile) throws IOException, InvalidFormatException {
-        //Each trailer will be linked by its identification number.
         //Process the excel file.
-        DataFormatter dataFormatter = new DataFormatter();
         Workbook workbook = WorkbookFactory.create(new File("./data/UnloadSchedule.xlsx"));
-        System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets.");
+
         Sheet sheet = workbook.getSheetAt(0);
-        for(Row row : sheet){
-            for(Cell cell : row){
-                System.out.println(dataFormatter.formatCellValue(cell));
-            }
+        Iterator<Row> rowIterator = sheet.rowIterator();
+
+        //Pre-process to row 7 for data.
+        for(int i = 1; i <= 6; i++) {
+            rowIterator.next();
         }
 
-        /*
-        Iterator<Row> rowIterator = sheet.rowIterator();
-        while (rowIterator.hasNext()) {
+        while(rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+
+            Iterator<Cell> cellIterator = row.cellIterator();
+
+            double num;
+            String str;
+
+
+            for(int i = 0; i <= 6; i++) {
+                Cell cell = cellIterator.next();
+
+                if(cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
+                    num = cell.getNumericCellValue();
+                } else if(cell.getCellType() == Cell.CELL_TYPE_STRING) {
+                    str = cell.getStringCellValue();
+                } else if(cell.getCellType() == Cell.CELL_TYPE_FORMULA) {
+                    switch (cell.getCachedFormulaResultType()) {
+                        case Cell.CELL_TYPE_NUMERIC:
+                            num = cell.getNumericCellValue();
+                            break;
+                        case Cell.CELL_TYPE_STRING:
+                            str = cell.getStringCellValue();
+                            break;
+                    }
+                }
+            }
+            //Column 1 : Trailer Number : String
+            //Column 2 : Origin Number : Double
+            //Column 3 : Volume Number : Double
+            //Column 4 : Smalls Number : Double
+            //Column 5 : Number of Bags : Double
+            //Column 6 : Number of Handles : Double
+            //Column 7 : Planned Hours : Double
+            //Anything beyond is inputed by the user, skip to the next line.
+
+
+        }
+        /*while (rowIterator.hasNext()) {
             Row row = rowIterator.next();
 
             Iterator<Cell> cellIterator = row.cellIterator();
             while (cellIterator.hasNext()) {
                 Cell cell = cellIterator.next();
-                String cellValue = dataFormatter.formatCellValue(cell);
-                System.out.print(cellValue);
+                double num = 0;
+                String str = "";
+                if(cell.getCellType() == Cell.CELL_TYPE_FORMULA) {
+                    switch(cell.getCachedFormulaResultType()) {
+                        case Cell.CELL_TYPE_NUMERIC:
+                            num = cell.getNumericCellValue();
+                            System.out.println(num);
+                            break;
+                        case Cell.CELL_TYPE_STRING:
+                            str = cell.getRichStringCellValue();
+                            System.out.println(str);
+                            break;
+                    }
+                } else {
+                    switch(cell.getCellType()) {
+                        case Cell.CELL_TYPE_NUMERIC:
+                            num = cell.getNumericCellValue();
+                            System.out.println(num);
+                            break;
+                        case Cell.CELL_TYPE_STRING:
+                            str = cell.getRichStringCellValue();
+                            System.out.println(str);
+                            break;
+                    }
+                }
+
+                //Column 1 : Trailer Number : String
+                //Column 2 : Origin Number : Double
+                //Column 3 : Volume Number : Double
+                //Column 4 : Smalls Number : Double
+                //Column 5 : Number of Bags : Double
+                //Column 6 : Number of Handles : Double
+                //Column 7 : Planned Hours : Double
+                //Anything beyond is inputed by the user, skip to the next line.
+
             }
-            System.out.println();
-        }
-        */
+        }*/
     }
 
     //Trims origin code into a 6 digit identification number.
