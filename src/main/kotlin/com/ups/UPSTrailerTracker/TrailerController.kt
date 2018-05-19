a0b83caa7aa076f83e8c1b42d8eddaa36cd4fb75
aaronshappell <crumster88@gmail.com>
2018-05-19 15:52:50 -0700

    Add trailer view test

--- src/main/kotlin/com/ups/UPSTrailerTracker/TrailerController.kt
+++ src/main/kotlin/com/ups/UPSTrailerTracker/TrailerController.kt
@@ -1,14 +1,26 @@
 package com.ups.UPSTrailerTracker
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.ResponseBody
 
-@RestController
+@Controller
 class TrailerController {
     @Autowired
     lateinit var trailerService: TrailerService
 
     @GetMapping("/trailers")
-    fun trailers(): ArrayList<Trailer> = trailerService.getTrailers()
+    fun getTrailersView(model: Model): String {
+        model.addAttribute("trailers", trailerService.getTrailers())
+        model.addAttribute("view", "trailers")
+        return "layout"
+    }
+
+    // Requires header "Accept:application/json"
+    @GetMapping("/trailers", produces = [MediaType.APPLICATION_JSON_UTF8_VALUE])
+    @ResponseBody
+    fun getTrailerJSON(): ArrayList<Trailer> = trailerService.getTrailers()
 }
