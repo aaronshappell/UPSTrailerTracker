7d9d2c7001b7f37014f1d0f9b62c982ec3e65830
aaronshappell <crumster88@gmail.com>
2018-05-20 22:58:12 -0700

    Add security test

---
+++ src/main/kotlin/com/ups/UPSTrailerTracker/WebSecurityConfig.kt
@@ -0,0 +1,39 @@
+package com.ups.UPSTrailerTracker
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests()
+                .antMatchers("/css/**", "/images/**", "/js/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/trailers")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+    }
+
+    @Bean
+    override fun userDetailsService(): UserDetailsService {
+        val user: UserDetails = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build()
+        return InMemoryUserDetailsManager(user)
+    }
+}
